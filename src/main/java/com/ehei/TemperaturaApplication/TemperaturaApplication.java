6fb107d8b91b551fbb53fbbce06137edfac63883
Othmane El Ahmadi <othmaneelahmadis@gmail.com>
2022-02-01 12:22:18 +0100

    Init Application

---
+++ src/main/java/com/ehei/TemperaturaApplication/TemperaturaApplication.java
@@ -0,0 +1,13 @@
+package com.ehei.TemperaturaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperaturaApplication.class, args);
+	}
+
+}
